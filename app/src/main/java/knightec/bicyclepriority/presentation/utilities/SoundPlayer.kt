2cb760eea7c0cd7c2cd189699ed9615d53adf93e
IsakHolm <holmdahl.isak@gmail.com>
2023-03-16 15:10:06 +0100

    Added sound for slowing down

--- app/src/main/java/knightec/bicyclepriority/presentation/utilities/SoundPlayer.kt
+++ app/src/main/java/knightec/bicyclepriority/presentation/utilities/SoundPlayer.kt
@@ -7,7 +7,7 @@ import knightec.bicyclepriority.R
 class SoundPlayer(private val context: Context) {
 
     fun testSound() {
-        val mediaPlayer = MediaPlayer.create(context, R.raw.leeroyjenkins)
+        val mediaPlayer = MediaPlayer.create(context, R.raw.slowdown)
         mediaPlayer.setOnCompletionListener {
             @Override
             fun onCompletion(mediaPlayer : MediaPlayer) {
