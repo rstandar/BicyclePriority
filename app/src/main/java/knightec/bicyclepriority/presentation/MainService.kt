80b416e1d8ab8dd31312e06aca3d3f9be30b93e6
rstandar <rassta@student.chalmers.se>
2023-04-17 15:18:11 +0200

    Added beep sound to test that foreground service is running
    Co-Authored-By: IsakHolm <98148805+IsakHolm@users.noreply.github.com>

--- app/src/main/java/knightec/bicyclepriority/presentation/MainService.kt
+++ app/src/main/java/knightec/bicyclepriority/presentation/MainService.kt
@@ -62,7 +62,7 @@ class MainService : Service() {
         val notification = NotificationCompat.Builder(this, "location").setContentTitle("Tracking...").setContentText("Location = null").setSmallIcon(R.mipmap.ic_launcher).setOngoing(true)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        locationClient.getLocationUpdates(1000L).catch { e -> e.printStackTrace() }.onEach {  location ->
+        locationClient.getLocationUpdates(2000).catch { e -> e.printStackTrace() }.onEach {  location ->
             val lat = location.latitude.toString()
             val long = location.longitude.toString()
             val speed = location.speed.toString()
@@ -70,6 +70,7 @@ class MainService : Service() {
             val updatedNotification = notification.setContentText("Lat is ($lat), long is ($long) and speed is ($speed)") //Used for foreground service
             getTrafficLightInformation(curLocation, prevLocation)
             notificationManager.notify(1, updatedNotification.build())
+            soundPlayer.beepSound()
             prevLocation = LocationDetails(lat,long,speed)
         }
             .launchIn(serviceScope)
--- app/src/main/java/knightec/bicyclepriority/presentation/utilities/SoundPlayer.kt
+++ app/src/main/java/knightec/bicyclepriority/presentation/utilities/SoundPlayer.kt
@@ -6,6 +6,11 @@ import knightec.bicyclepriority.R
 
 class SoundPlayer(private val context: Context) {
 
+    val beepPlayer = MediaPlayer.create(context, R.raw.beep)
+    fun beepSound() {
+        beepPlayer.start()
+    }
+
     fun slowDownSound() {
         val mediaPlayer = MediaPlayer.create(context, R.raw.decrease_your_speed_male)
         mediaPlayer.setOnCompletionListener {
